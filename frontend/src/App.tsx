f302fcb57c0f69282fdcbd8d96d0bf03db216612
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-15 11:40:20 +0900

    Chakra UIの導入

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
+import { Home } from './components/pages/Home'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ChakraProvider>
+      <Home />
+    </ChakraProvider>
+  )
 }
-
-export default App;
