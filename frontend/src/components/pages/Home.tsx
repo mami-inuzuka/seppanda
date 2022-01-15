c2d90f327fbc8d226d8572fb5ecc5c49fb0f5117
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-15 14:22:44 +0900

    PaymentListコンポーネントの作成

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,3 +1,43 @@
 import { memo, VFC } from 'react'
 
-export const Home: VFC = memo(() => <p>HOMEページです</p>)
+import { Table, Tbody } from '@chakra-ui/react'
+import { Payment } from '../../types/payment'
+import { PaymentList } from '../organisms/PaymentList'
+
+export const Home: VFC = memo(() => {
+  const paymentsData: Array<Payment> = [
+    {
+      amount: 600,
+      user: 'hanako',
+      created_at: '2022-01-15',
+    },
+    {
+      amount: 1000,
+      user: 'hanako',
+      created_at: '2022-01-15',
+    },
+    {
+      amount: 950,
+      user: 'taro',
+      created_at: '2022-01-15',
+    },
+    {
+      amount: 1280,
+      user: 'hanako',
+      created_at: '2022-01-13',
+    },
+  ]
+  return (
+    <>
+      <div>せっぱんだ</div>
+      <div>32,670</div>
+      {paymentsData.map((data) => (
+        <Table variant="simple">
+          <Tbody>
+            <PaymentList amount={data.amount} user={data.user} date={data.created_at} />
+          </Tbody>
+        </Table>
+      ))}
+    </>
+  )
+})
