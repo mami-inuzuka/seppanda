6ccdfcf5a48cb1fc2caa6ae941090c8d92f8720c
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-15 19:18:52 +0900

    devise_auth_tokenを使用してユーザー登録、ログインに関するAPIを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+        include ActionController::Helpers
+        include DeviseTokenAuth::Concerns::SetUserByToken
+
+        skip_before_action :verify_authenticity_token, raise: false
+        helper_method :current_user, :user_signed_in?
 end
