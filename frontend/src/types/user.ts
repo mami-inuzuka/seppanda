4e9afb5d0918bb54672ff83e183c47d45e9e4d31
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-18 12:55:15 +0900

    userの型を定義

---
+++ frontend/src/types/user.ts
@@ -0,0 +1,12 @@
+export type User = {
+  id: number
+  uid: string
+  provider: string
+  email: string
+  name: string
+  nickname?: string
+  image?: string
+  allowPasswordChange: boolean
+  created_at: Date
+  updated_at: Date
+}
