fcbc986d0076f253a362c964aac3e15f1ca572b2
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-19 23:47:21 +0900

    SecondaryButtonコンポーネントの作成

---
+++ frontend/src/components/atoms/button/SecondaryButton.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@chakra-ui/react'
+import { FC, ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode
+}
+
+export const SecondaryButton: FC<Props> = (props) => {
+  const { children } = props
+  return (
+    <Button
+      bg="gray.50"
+      color="gray.900"
+      borderRadius="base"
+      flex="1"
+      h="64px"
+      w="100%"
+      border="1px solid rgba(46, 47, 46, 0.1)"
+      boxShadow="0px 1px 0px #D7D7D7"
+    >
+      {children}
+    </Button>
+  )
+}
