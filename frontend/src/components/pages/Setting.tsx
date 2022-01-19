30ee73c7a23245beb0d17c0aa9af97232954ccc3
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-19 23:47:49 +0900

    設定画面UIの作成

---
+++ frontend/src/components/pages/Setting.tsx
@@ -0,0 +1,22 @@
+import { Flex, FormControl, FormLabel, Image, Input } from '@chakra-ui/react'
+import { VFC } from 'react'
+import { PrimaryButton } from '../atoms/button/PrimaryButton'
+import { SecondaryButton } from '../atoms/button/SecondaryButton'
+import { UserIcon } from '../atoms/icon/UserIcon'
+
+export const Setting: VFC = () => (
+  <>
+    <Flex>
+      <UserIcon src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+      <SecondaryButton>画像を選択</SecondaryButton>
+    </Flex>
+    <FormControl>
+      <FormLabel htmlFor="email">メールアドレス</FormLabel>
+      <Input id="email" type="email" value="taro@example.com" size="lg" />
+      <FormLabel htmlFor="name">名前</FormLabel>
+      <Input id="name" value="taro" size="lg" />
+    </FormControl>
+
+    <PrimaryButton>保存する</PrimaryButton>
+  </>
+)
