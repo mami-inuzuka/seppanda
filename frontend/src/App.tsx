cc2f7993190fedc271f7a2b720bf072821f3dc4a
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-19 10:49:54 +0900

    サインイン機能の作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,40 @@
+import { createContext, useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import { ChakraProvider } from '@chakra-ui/react'
 
-import { Home } from './components/pages/Home'
+/* eslint import/no-cycle: 0 */
+import { SignIn } from './components/pages/SignIn'
 import { HeaderLayout } from './components/templates/HeaderLayout'
 import { theme } from './theme'
+import { User } from './types/user'
+
+export const AuthContext = createContext(
+  {} as {
+    isSignedIn: boolean | undefined
+    setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>
+    currentUser: User | undefined
+    setCurrentUser: React.Dispatch<React.SetStateAction<User | undefined>>
+  }
+)
 
 export default function App() {
+  // サインインしているかどうかをtrue/falseで判別する
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false)
+  const [currentUser, setCurrentUser] = useState<User | undefined>()
+
   return (
-    <ChakraProvider theme={theme}>
-      <HeaderLayout>
-        <Home />
-      </HeaderLayout>
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        {/* eslint react/jsx-no-constructed-context-values: 0 */}
+        <AuthContext.Provider value={{ isSignedIn, setIsSignedIn, currentUser, setCurrentUser }}>
+          <HeaderLayout>
+            <Routes>
+              <Route path="/signin" element={<SignIn />} />
+            </Routes>
+          </HeaderLayout>
+        </AuthContext.Provider>
+      </ChakraProvider>
+    </BrowserRouter>
   )
 }
