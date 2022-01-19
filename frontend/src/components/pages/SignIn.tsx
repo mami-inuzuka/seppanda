1486c584f40c801d72eb702ccf35fb9cdf41f3ba
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-19 10:49:54 +0900

    サインイン機能の作成

---
+++ frontend/src/components/pages/SignIn.tsx
@@ -0,0 +1,63 @@
+import { useState, useContext, VFC, memo } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+import { Box, Button, Input } from '@chakra-ui/react'
+
+/* eslint import/no-cycle: 0 */
+import { AuthContext } from '../../App'
+
+import { signIn } from '../../lib/api/auth'
+import { SignInParams } from '../../types/signInParams'
+
+export const SignIn: VFC = memo(() => {
+  const navigate = useNavigate()
+
+  const { setIsSignedIn, setCurrentUser, currentUser } = useContext(AuthContext)
+
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    // デフォルトの動作をキャンセル
+    e.preventDefault()
+
+    const params: SignInParams = {
+      email,
+      password,
+    }
+
+    try {
+      const res = await signIn(params)
+      console.log(res)
+      if (res.status === 200) {
+        // ログインに成功した場合はCookieに各値を格納
+        Cookies.set('_access_token', res.headers['access-token'])
+        Cookies.set('_client', res.headers.client)
+        Cookies.set('_uid', res.headers.uid)
+        setIsSignedIn(true)
+        setCurrentUser(res.data.data)
+        navigate('/')
+        console.log('Signed in successfully!')
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <form noValidate autoComplete="off">
+      <Box>
+        <Input value={email} onChange={(event) => setEmail(event.target.value)} />
+        <Input value={password} onChange={(event) => setPassword(event.target.value)} />
+        <Button
+          type="submit"
+          disabled={!!(!email || !password)} // 空欄があった場合はボタンを押せないように
+          onClick={handleSubmit}
+        >
+          Submit
+        </Button>
+      </Box>
+    </form>
+  )
+})
