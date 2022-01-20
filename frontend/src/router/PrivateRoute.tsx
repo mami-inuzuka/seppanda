3c0d48f0e26a592a80576b7a1146bc543ecc209a
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-20 23:38:00 +0900

    no-else-returnのESLintエラーを解消

--- frontend/src/router/PrivateRoute.tsx
+++ frontend/src/router/PrivateRoute.tsx
@@ -5,10 +5,6 @@ import { AuthContext } from '../context/AuthContext'
 
 export const PrivateRoute = ({ children }: { children: React.ReactElement }) => {
   const { isSignedIn } = useContext(AuthContext)
-  if (isSignedIn) {
-    return children
-  } else {
-    /* eslint no-else-return:0 */
-    return <Redirect to="/signin" />
-  }
+  if (isSignedIn) return children
+  return <Redirect to="/signin" />
 }
