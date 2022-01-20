795f3cfa07ecead7d85f723358aa81f45e51110d
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-20 22:30:36 +0900

    Rubocopエラーの修正

--- app/controllers/api/auth/sessions_controller.rb
+++ app/controllers/api/auth/sessions_controller.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class Api::Auth::SessionsController < ApplicationController
   def index
     if current_api_user
       render json: { is_login: true, data: current_api_user }
     else
-      render json: { is_login: false, message: "ユーザーが存在しません" }
+      render json: { is_login: false, message: 'ユーザーが存在しません' }
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-        include ActionController::Helpers
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include ActionController::Helpers
+  include DeviseTokenAuth::Concerns::SetUserByToken
 
-        skip_before_action :verify_authenticity_token, raise: false
-        helper_method :current_user, :user_signed_in?
+  skip_before_action :verify_authenticity_token, raise: false
+  helper_method :current_user, :user_signed_in?
 end
