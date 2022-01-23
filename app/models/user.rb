2dc844f3be6ccceee4371e4fcaee2063a265a44e
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-23 10:46:19 +0900

    UserモデルとTeamモデルを関連付ける

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  belongs_to :team, optional: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
