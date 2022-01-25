8a1791577ec5d271df2edae7bdec11f1a01b4a52
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-25 22:09:45 +0900

    API通信が完了してからルーティングの分岐を判定する

--- frontend/src/router/PrivateRoute.tsx
+++ frontend/src/router/PrivateRoute.tsx
@@ -4,7 +4,10 @@ import { Redirect } from 'react-router-dom'
 import { AuthContext } from '../context/AuthContext'
 
 export const PrivateRoute = ({ children }: { children: React.ReactElement }) => {
-  const { isSignedIn } = useContext(AuthContext)
-  if (isSignedIn) return children
-  return <Redirect to="/signin" />
+  const { isLoaded, isSignedIn } = useContext(AuthContext)
+  if (isLoaded) {
+    if (isSignedIn) return children
+    return <Redirect to="/signin" />
+  }
+  return null
 }
