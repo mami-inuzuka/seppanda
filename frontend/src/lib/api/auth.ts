bdbdcb0dbfe337d2e51481a4f450d76efabd7782
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-25 10:07:26 +0900

    認証済みユーザーの取得をするaxiosでのリクエストのレスポンスに型を定義する

--- frontend/src/lib/api/auth.ts
+++ frontend/src/lib/api/auth.ts
@@ -5,6 +5,7 @@ import client from './client'
 import { SignUpParams } from '../../types/signUpParams'
 import { SignInParams } from '../../types/signInParams'
 import { SignUpResponse } from '../../types/signUpResponse'
+import { CurrentUserResponse } from '../../types/currentUserResponse'
 
 export const signUp = (params: SignUpParams, token?: string | null): AxiosPromise<SignUpResponse> =>
   client.post('auth', params, { headers: { ParingToken: token || '' } })
@@ -21,8 +22,8 @@ export const signOut = () =>
   })
 
 // 認証済みのユーザーを取得
-export const getCurrentUser = () => {
-  if (!Cookies.get('_access_token') || !Cookies.get('_client') || !Cookies.get('_uid')) return false
+export const getCurrentUser = (): AxiosPromise<CurrentUserResponse> | undefined => {
+  if (!Cookies.get('_access_token') || !Cookies.get('_client') || !Cookies.get('_uid')) return undefined
   return client.get('/auth/sessions', {
     headers: {
       'access-token': Cookies.get('_access_token') || '',
