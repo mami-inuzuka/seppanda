c5da65bd302bad8f0c9af3df2195cd851577e8b9
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-26 08:32:04 +0900

    routerディレクトリをcomponents配下に移動

--- frontend/src/components/router/PrivateRoute.tsx
+++ frontend/src/components/router/PrivateRoute.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 import { Redirect } from 'react-router-dom'
 
-import { AuthContext } from '../context/AuthContext'
+import { AuthContext } from '../../context/AuthContext'
 
 export const PrivateRoute = ({ children }: { children: React.ReactElement }) => {
   const { isLoaded, isSignedIn } = useContext(AuthContext)
