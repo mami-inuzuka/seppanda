358dcbfd57604e7ee0d8ef9a532d1eb38252561e
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-25 21:47:21 +0900

    AuthContextにisLoadedとsetIsLoadedを追加し、それぞれの型と初期値を設定し直す

--- frontend/src/context/AuthContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-
-export const AuthContext = createContext(
-  {} as {
-    isSignedIn: boolean | undefined
-    setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>
-  }
-)
