16226ff9dd8c2ce2a5023a82d925271138d4f78f
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-25 10:11:13 +0900

    アプリアクセス時に認証済みのユーザーがいるかどうかを確認する

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
-import { useMemo, useState, VFC } from 'react'
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { useEffect, useMemo, useState, VFC } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 
@@ -10,6 +12,8 @@ import { AuthContext } from './context/AuthContext'
 import { PrivateRoute } from './router/PrivateRoute'
 import { theme } from './theme'
 import { Paring } from './components/pages/Paring'
+import { getCurrentUser } from './lib/api/auth'
+import { User } from './types/user'
 
 // ユーザーが認証済みかどうかでルーティングを決定
 // 未認証だった場合は「/signin」ページに促す
@@ -22,6 +26,32 @@ const App: VFC = () => {
     }),
     [isSignedIn]
   )
+
+  const [currentUser, setCurrentUser] = useState<User | undefined>()
+
+  // 認証済みのユーザーがいるかどうかチェック
+  const handleGetCurrentUser = async () => {
+    try {
+      const res = await getCurrentUser()
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      console.log('getCurrentUserのres ')
+      console.log(res?.data)
+      if (res?.data.isLogin === true) {
+        setIsSignedIn(true)
+        setCurrentUser(res?.data.data)
+        console.log(isSignedIn)
+      } else {
+        console.log('No current user')
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    handleGetCurrentUser()
+  }, [setCurrentUser])
 
   return (
     <BrowserRouter>
