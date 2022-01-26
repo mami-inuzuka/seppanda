e5bb3f4c4878ad4c13a924d090c325f277a15f10
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-26 16:00:44 +0900

    クリックしたボタンの値をディスプレイに表示する

--- frontend/src/components/organisms/Calculator/Button.tsx
+++ frontend/src/components/organisms/Calculator/Button.tsx
@@ -5,12 +5,13 @@ import { Button } from '@chakra-ui/react'
 type Props = {
   label: string
   value: string
+  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
 export const CalculatorButton: VFC<Props> = (props) => {
-  const { label, value } = props
+  const { label, value, onClick } = props
   return (
-    <Button data-value={value} bg="gray.50" h="100%" fontSize="32px">
+    <Button data-value={value} bg="gray.50" h="100%" fontSize="32px" onClick={onClick}>
       {label}
     </Button>
   )
