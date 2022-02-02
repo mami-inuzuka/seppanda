e6a17152defa8f6cffd13c0adb5991535ee55e87
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-02 12:35:04 +0900

    optional trueの設定を外す

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  belongs_to :team, optional: true
+  belongs_to :team
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
