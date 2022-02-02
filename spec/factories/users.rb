1b89fcb4001449d8b672de8507fd19ef76e3bd6a
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-02 13:11:18 +0900

    userファクトリの追加

---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :first_user, class: 'User' do
+    name { "first_user" }
+    email { "first_user@example.com" }
+    password { "testtest" }
+    association :team
+  end
+
+  # 2人目の場合はsave前にinvitation_tokenをもとにteamを紐づけるため新たにteamを作成しない
+  # そのため association :team の設定を外している
+  factory :second_user, class: 'User' do
+    name { "second_user" }
+    email { "second_user@example.com" }
+    password { "testtest" }
+  end
+end
