696e56fb3a90480522d7b4dfdbbea4dc086590d8
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-02 12:40:29 +0900

    invitation_tokenのセットをbuild_resourceメソッドの中に含める

--- app/controllers/api/auth/registrations_controller.rb
+++ app/controllers/api/auth/registrations_controller.rb
@@ -3,20 +3,29 @@
 class Api::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   before_action :check_invitation_token, only: :create
 
-  def create
+  private
+
+  def build_resource
+    @resource            = resource_class.new(sign_up_params)
+    @resource.provider   = provider
+
+    # honor devise configuration for case_insensitive_keys
+    if resource_class.case_insensitive_keys.include?(:email)
+      @resource.email = sign_up_params[:email].try(:downcase)
+    else
+      @resource.email = sign_up_params[:email]
+    end
+
+    # 以下追記
     invitation_token = request.headers[:InvitationToken]
-    super do |resource|
-      if invitation_token.present?
-        team = Team.find_by!(invitation_token: invitation_token)
-        resource.team_id = team.id
-      else
-        @invitation_token = SecureRandom.urlsafe_base64
-        resource.create_team!(invitation_token: @invitation_token)
-      end
+    if invitation_token.present?
+      team = Team.find_by!(invitation_token: invitation_token)
+      @resource.team_id = team.id
+    else
+      @invitation_token = SecureRandom.urlsafe_base64
+      @resource.create_team!(invitation_token: @invitation_token)
     end
   end
-
-  private
 
   def render_create_success
     if Team.enabled?(resource_data["team_id"])
