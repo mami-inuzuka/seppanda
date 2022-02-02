366ac89b16a6f61f4e1f07ef358191240e2fa06d
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-02 22:21:21 +0900

    共通の処理をletやsubjectでまとめる

--- spec/requests/auth_api_spec.rb
+++ spec/requests/auth_api_spec.rb
@@ -1,77 +1,67 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "AuthApi", type: :request do
-  describe "POST api_user_registration_path" do
-    context "without invitation_token" do
-      it "creates a new user having a new team with invitation token" do
-        headers = {
-          'Content-Type'=>'application/json',
-          'Accept'=> 'application/json',
-          'InvitationToken'=> ''
-        }
-        expect {
-          post api_user_registration_path, params: {
-            name: 'charlie',
-            email: 'charlie@example.com',
-            password: 'testtest',
-            password_confirmation: 'testtest'
-          }.to_json, headers: headers
-        }.to change(User, :count).by(1)
-        user = User.find_by(email: 'charlie@example.com')
-        expect(user.team.invitation_token).not_to be_nil
-      end
+RSpec.describe 'AuthApi', type: :request do
+  describe 'POST api_user_registration_path' do
+    subject(:execute_post) { post api_user_registration_path, params: new_user_params, headers: headers }
 
-      it "returns json including invitation token" do
-        headers = {
-          'Content-Type'=>'application/json',
-          'Accept'=> 'application/json',
-          'InvitationToken'=> ''
+    context 'without invitation_token' do
+      let(:headers) {
+        {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          InvitationToken: ''
         }
-        post api_user_registration_path, params: {
+      }
+      let(:new_user_params) {
+        {
           name: 'charlie',
           email: 'charlie@example.com',
           password: 'testtest',
           password_confirmation: 'testtest'
-        }.to_json, headers: headers
+        }.to_json
+      }
+
+      it 'creates a new user having a new team with invitation token' do
+        expect { execute_post }.to change(User, :count).by(1)
+        user = User.find_by(email: 'charlie@example.com')
+        expect(user.team.invitation_token).not_to be_nil
+      end
+
+      it 'returns json including invitation token' do
+        execute_post
         expect(JSON.parse(response.body)['invitation_token']).not_to be_empty
       end
     end
 
-    context "with invitation_token" do
-      it 'creates a new user belongs to a team same with invitee' do
-        first_user = create(:first_user)
-        invitation_token = first_user.team.invitation_token
-        headers = {
-          'Content-Type'=>'application/json',
-          'Accept'=> 'application/json',
-          'InvitationToken'=> invitation_token
-        }
-        expect {
-          post api_user_registration_path, params: {
-            name: 'bob',
-            email: 'bob@example.com',
-            password: 'testtest',
-            password_confirmation: 'testtest'
-          }.to_json, headers: headers
-        }.to change(User, :count).by(1)
-        second_user = User.find_by(email: 'bob@example.com')
-        expect(first_user.team_id).to eq second_user.team_id
-      end
-
-      it "returns json including empty invitation token" do
-        first_user = create(:first_user)
-        invitation_token = first_user.team.invitation_token
-        headers = {
-          'Content-Type'=>'application/json',
-          'Accept'=> 'application/json',
-          'InvitationToken'=> invitation_token
-        }
-        post api_user_registration_path, params: {
+    context 'with invitation_token' do
+      let!(:first_user) { create(:first_user) }
+      let!(:invitation_token) { first_user.team.invitation_token }
+      let(:new_user_params) {
+        {
           name: 'bob',
           email: 'bob@example.com',
           password: 'testtest',
           password_confirmation: 'testtest'
-        }.to_json, headers: headers
+        }.to_json
+      }
+      let(:headers) {
+        {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          InvitationToken: invitation_token
+        }
+      }
+
+      it 'creates a new user belongs to a team same with invitee' do
+        expect { execute_post }.to change(User, :count).by(1)
+        second_user = User.find_by(email: 'bob@example.com')
+        expect(first_user.team_id).to eq second_user.team_id
+      end
+
+      it 'returns json including empty invitation token' do
+        execute_post
         expect(JSON.parse(response.body)['invitation_token']).to be_empty
       end
     end
