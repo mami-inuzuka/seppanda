30cf16ee75d2dbb701f3eb6994bcb91e1fd801c8
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-07 15:20:45 +0900

    Teamモデルのインスタンスメソッドの命名を変更したのに合わせてテストを修正

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -3,11 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  example '1つのteamに含まれるユーザーが2人の時teamは有効な状態となる' do
+  example '1つのteamに含まれるユーザーが2人の時満員となる' do
     host_user = create(:user, :with_team)
     guest_user = build(:user)
     guest_user.team_id = host_user.team_id
     guest_user.save
-    expect(Team.enabled?(host_user.team_id)).to be true
+    expect(host_user.team.capacity_reached?).to be true
   end
 end
