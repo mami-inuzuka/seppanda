e3fb8870c642e85359eff95d49cabd443b0d214e
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-07 19:22:10 +0900

    amountカラムにバリデーションを設定

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,6 @@
 class Payment < ApplicationRecord
   belongs_to :team
   belongs_to :user
+
+  validates :amount, format: { with: /\A[0-9]+\z/ }, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 9999999 }
 end
