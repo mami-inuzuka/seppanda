f028a649bdf0c96b72922e5b3adc373fc0a738b9
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-09 08:06:39 +0900

    相対パスを絶対パスに変更

--- frontend/src/context/PaymentContext.ts
+++ frontend/src/context/PaymentContext.ts
@@ -1,6 +1,6 @@
 import { createContext } from 'react'
 
-import type { Payment } from '../types/payment'
+import type { Payment } from 'types/payment'
 
 export type PaymentContextType = {
   inputNumber: string
