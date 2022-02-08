3922348a35e3ee31df043e3179cdcef4c64678dd
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-08 13:55:40 +0900

    lintの指摘を修正

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -40,6 +40,7 @@ export const Home: VFC = memo(() => {
     handleGetPayments().catch((err) => {
       console.log(err)
     })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
