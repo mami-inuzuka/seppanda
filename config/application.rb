2ed32bca4f5df55e24d32e6566266182bf9fac87
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-08 11:19:42 +0900

    タイムゾーンを日本に設定する

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Seppanda
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Tokyo'
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
