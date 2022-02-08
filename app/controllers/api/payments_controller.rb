950da26d9b4435b98486c41512611dfd846a121f
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-08 13:53:29 +0900

    Rubocopエラーの解消

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Api::PaymentsController < ApplicationController
   before_action :authenticate_api_user!
 
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Payment < ApplicationRecord
   belongs_to :team
   belongs_to :user
 
-  validates :amount, format: { with: /\A[0-9]+\z/ }, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 9999999 }
+  validates :amount, format: { with: /\A[0-9]+\z/ }, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 9_999_999 }
 end
