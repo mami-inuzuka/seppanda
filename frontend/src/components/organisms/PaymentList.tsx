0218c774d9f735781f3ea8c847a3ef5ae91833db
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-09 16:03:28 +0900

    各支払い情報の行をタップすると入力画面が出るようにする

--- frontend/src/components/organisms/PaymentList.tsx
+++ frontend/src/components/organisms/PaymentList.tsx
@@ -1,4 +1,5 @@
 import { memo, VFC } from 'react'
+import { useHistory } from 'react-router-dom'
 
 import { Table, Tbody, Td, Tr } from '@chakra-ui/react'
 import { DateTime } from 'luxon'
@@ -11,11 +12,17 @@ type Props = {
 
 export const PaymentList: VFC<Props> = memo((props) => {
   const { payments } = props
+  const history = useHistory()
+  const handleRowClick = (payment: Payment) =>
+    history.push({
+      pathname: `/payments/${payment.id}`,
+      state: { payment },
+    })
   return (
     <Table variant="simple">
       <Tbody>
         {payments.map((payment) => (
-          <Tr key={payment.id}>
+          <Tr key={payment.id} onClick={() => handleRowClick(payment)}>
             <Td>{DateTime.fromISO(payment.createdAt).toISODate()}</Td>
             <Td>{payment.user.name}</Td>
             <Td isNumeric>{payment.amount}</Td>
---
+++ frontend/src/components/pages/ShowPaymentEntry.tsx
@@ -0,0 +1,73 @@
+import { useContext, useEffect, VFC } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
+
+import { Box, CloseButton, Flex } from '@chakra-ui/react'
+
+import { BarButton } from 'components/atoms/button/BarButton'
+import { Calculator } from 'components/organisms/Calculators/Calculator'
+import { PaymentContext } from 'context/PaymentContext'
+import { updatePayment } from 'lib/api/payment'
+import { useToast } from 'lib/toast'
+
+import type { Payment } from 'types/payment'
+import type { PostPaymentParams } from 'types/postPaymentParams'
+
+type stateType = {
+  payment: Payment
+}
+
+export const ShowPaymentEntry: VFC = () => {
+  const { inputNumber, setInputNumber, paymentList, setPaymentList } = useContext(PaymentContext)
+  const { errorToast, successToast } = useToast()
+  const history = useHistory()
+  const location = useLocation()
+  const state = location.state as stateType
+  const { payment } = state
+
+  const onClickClose = () => {
+    history.push('/')
+  }
+
+  const handleUpdateAmount = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+
+    const params: PostPaymentParams = {
+      amount: Number(inputNumber),
+    }
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+      const res = await updatePayment(params, payment.id)
+      if (res.status === 200) {
+        const newPaymentList = paymentList != null ? [res.data, ...paymentList] : [res.data]
+        setPaymentList(newPaymentList)
+        onClickClose()
+        successToast('支払い情報を更新しました')
+        setInputNumber('0')
+      } else {
+        errorToast('更新に失敗しました')
+      }
+    } catch {
+      errorToast('更新に失敗しました')
+    }
+  }
+
+  useEffect(() => {
+    setInputNumber(String(payment.amount))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <Flex flexDirection="column" h="100vh">
+      <Box flex="1">
+        <Calculator />
+      </Box>
+      <Flex h="64px">
+        <CloseButton onClick={() => onClickClose()} />
+        <BarButton onClickButton={handleUpdateAmount} disabled={inputNumber === '0'}>
+          更新する
+        </BarButton>
+      </Flex>
+    </Flex>
+  )
+}
