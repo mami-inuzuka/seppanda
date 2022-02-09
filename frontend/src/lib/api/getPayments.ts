637c11c84752ac43685cfdd8598eee4496175804
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-09 09:36:47 +0900

    相対パスを絶対パスに変更

--- frontend/src/lib/api/getPayments.ts
+++ frontend/src/lib/api/getPayments.ts
@@ -1,9 +1,9 @@
 import { AxiosPromise } from 'axios'
 import Cookies from 'js-cookie'
 
-import client from './client'
+import client from 'lib/api/client'
 
-import type { Payment } from '../../types/payment'
+import type { Payment } from 'types/payment'
 
 export const getPayments = (): AxiosPromise<Payment[] | null> =>
   client.get('/payments', {
--- frontend/src/lib/api/postPayment.ts
+++ frontend/src/lib/api/postPayment.ts
@@ -1,10 +1,10 @@
 import { AxiosPromise } from 'axios'
 import Cookies from 'js-cookie'
 
-import client from './client'
+import client from 'lib/api/client'
 
-import type { Payment } from '../../types/payment'
-import type { PostPaymentParams } from '../../types/postPaymentParams'
+import type { Payment } from 'types/payment'
+import type { PostPaymentParams } from 'types/postPaymentParams'
 
 export const postPayment = (params: PostPaymentParams): AxiosPromise<Payment> =>
   client.post('/payments', params, {
