0c0a05c609a8a21ddc0f194e4b74810096aabf60
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-09 17:56:47 +0900

    allback_index_htmlを定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
   include ActionController::Helpers
   include DeviseTokenAuth::Concerns::SetUserByToken
 
   skip_before_action :verify_authenticity_token, raise: false
+
+  def fallback_index_html
+    respond_to do |format|
+      format.html { render body: Rails.root.join('public/index.html').read }
+    end
+  end
 end
