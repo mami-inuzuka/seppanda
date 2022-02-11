fdbeb3920860b6e4840e0478ccef0cea3681df2d
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-11 18:49:57 +0900

    計算に必要なメソッドの定義とテストの作成

--- app/models/team.rb
+++ app/models/team.rb
@@ -7,6 +7,28 @@ class Team < ApplicationRecord
   # 現在は1teamにつき2人まで登録できるようにする
   MAX_TEAM_MENBER_NUMBER = 2
 
+  def total_amount
+    self.payments.sum(:amount)
+  end
+
+  def payment_per_person
+    self.total_amount / MAX_TEAM_MENBER_NUMBER
+  end
+
+  def refund_amount
+    self.largest_payment_user.payments.sum(:amount) - self.payment_per_person
+  end
+
+  def largest_payment_user
+    user_and_total_amount = users.map { |user| [user.id, user.payments.sum(:amount)]}.to_h
+    User.find(user_and_total_amount.key(user_and_total_amount.values.max))
+  end
+
+  def smallest_payment_user
+    user_and_total_amount = users.map { |user| [user.id, user.payments.sum(:amount)]}.to_h
+    User.find(user_and_total_amount.key(user_and_total_amount.values.min))
+  end
+
   def capacity_reached?
     users.length == MAX_TEAM_MENBER_NUMBER
   end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -3,16 +3,53 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  example '1つのteamに含まれるユーザーが2人の時満員となる' do
-    host_user = create(:user, :with_team)
-    guest_user = build(:user)
-    guest_user.team_id = host_user.team_id
-    guest_user.save
-    expect(host_user.team.capacity_reached?).to be true
+  context '人数制限' do
+    example '1つのteamに含まれるユーザーが2人の時満員となる' do
+      host_user = create(:user, :with_team)
+      guest_user = build(:user)
+      guest_user.team_id = host_user.team_id
+      guest_user.save
+      expect(host_user.team.capacity_reached?).to be true
+    end
+
+    example '1つのteamに含まれるユーザーが1人の時は満員ではない' do
+      host_user = create(:user, :with_team)
+      expect(host_user.team.capacity_reached?).to be false
+    end
   end
 
-  example '1つのteamに含まれるユーザーが1人の時は満員ではない' do
-    host_user = create(:user, :with_team)
-    expect(host_user.team.capacity_reached?).to be false
+  context '支払い金額' do
+    let!(:user_1) { create(:user, :with_team) }
+    let!(:user_2) { create(:user, team_id: user_1.team_id) }
+    let!(:team) { user_1.team }
+    let!(:payment_1) { user_1.payments.create(amount: 800, team_id: team.id) }
+    let!(:payment_2) { user_1.payments.create(amount: 200, team_id: team.id) }
+    let!(:payment_3) { user_2.payments.create(amount: 100, team_id: team.id) }
+    let!(:payment_4) { user_2.payments.create(amount: 300, team_id: team.id) }
+
+    example '各ユーザーの合計金額' do
+      expect(user_1.payments.sum(:amount)).to eq 1000
+      expect(user_2.payments.sum(:amount)).to eq 400
+    end
+
+    example 'teamの合計金額' do
+      expect(team.total_amount).to eq 1400
+    end
+
+    example '1人あたり支払うべき金額' do
+      expect(team.payment_per_person).to eq 700
+    end
+
+    example '返金額' do
+      expect(team.refund_amount).to eq 300
+    end
+
+    example '一人当たり支払うべき金額より多く支払っているユーザー' do
+      expect(team.largest_payment_user).to eq user_1
+    end
+
+    example '一人当たり支払うべき金額より支払いが少ないユーザー' do
+      expect(team.smallest_payment_user).to eq user_2
+    end
   end
 end
