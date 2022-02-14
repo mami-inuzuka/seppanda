50fa08d6e81a13d6d073f4801e2de6f8c5198613
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-14 14:52:09 +0900

    inputのvalueに現在ログイン中のユーザー情報をセットする

--- frontend/src/components/pages/Setting.tsx
+++ frontend/src/components/pages/Setting.tsx
@@ -1,23 +1,42 @@
-import { VFC } from 'react'
+import { useContext, useEffect, useState, VFC } from 'react'
 
 import { Flex, FormControl, FormLabel, Input } from '@chakra-ui/react'
 
 import { PrimaryButton } from 'components/atoms/button/PrimaryButton'
 import { SecondaryButton } from 'components/atoms/button/SecondaryButton'
 import { UserIcon } from 'components/atoms/icon/UserIcon'
+import { AuthContext } from 'context/AuthContext'
 
-export const Setting: VFC = () => (
-  <>
-    <Flex>
-      <UserIcon src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
-      <SecondaryButton>画像を選択</SecondaryButton>
-    </Flex>
-    <FormControl>
-      <FormLabel htmlFor="email">メールアドレス</FormLabel>
-      <Input id="email" type="email" value="taro@example.com" size="lg" />
-      <FormLabel htmlFor="name">名前</FormLabel>
-      <Input id="name" value="taro" size="lg" />
-    </FormControl>
+export const Setting: VFC = () => {
+  const { currentUser } = useContext(AuthContext)
+  const [inputName, setInputName] = useState<string>('')
+  const [inputEmail, setInputEmail] = useState<string>('')
+
+  useEffect(() => {
+    if (currentUser) {
+      setInputName(currentUser.name)
+      setInputEmail(currentUser.email)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return (
+    <>
+      <Flex>
+        <UserIcon src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+        <SecondaryButton>画像を選択</SecondaryButton>
+      </Flex>
+      <FormControl>
+        <FormLabel htmlFor="name">名前</FormLabel>
+        <Input value={inputName} onChange={(event) => setInputName(event.target.value)} id="name" size="lg" />
+        <FormLabel htmlFor="email">メールアドレス</FormLabel>
+        <Input
+          value={inputEmail}
+          onChange={(event) => setInputEmail(event.target.value)}
+          id="email"
+          type="email"
+          size="lg"
+        />
+      </FormControl>
 
     <PrimaryButton>保存する</PrimaryButton>
   </>
