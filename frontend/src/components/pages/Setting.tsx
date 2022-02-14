c4ce2183dda8e7fa3c7a7db90d8a598ce7570cb0
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-14 14:57:02 +0900

    保存ボタンをクリックしてユーザー情報を更新する

--- frontend/src/components/pages/Setting.tsx
+++ frontend/src/components/pages/Setting.tsx
@@ -6,11 +6,35 @@ import { PrimaryButton } from 'components/atoms/button/PrimaryButton'
 import { SecondaryButton } from 'components/atoms/button/SecondaryButton'
 import { UserIcon } from 'components/atoms/icon/UserIcon'
 import { AuthContext } from 'context/AuthContext'
+import { updateUser } from 'lib/api/auth'
+import { useToast } from 'lib/toast'
+import { UpdateUserParams } from 'types/updateUserParams'
 
 export const Setting: VFC = () => {
   const { currentUser } = useContext(AuthContext)
   const [inputName, setInputName] = useState<string>('')
   const [inputEmail, setInputEmail] = useState<string>('')
+  const { errorToast, successToast } = useToast()
+
+  const handleUpdateUser = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+
+    const params: UpdateUserParams = {
+      name: inputName,
+      email: inputEmail,
+    }
+
+    try {
+      const res = await updateUser(params)
+      if (res.status === 200) {
+        successToast('ユーザー情報を更新しました')
+      } else {
+        errorToast('ユーザー情報の更新に失敗しました')
+      }
+    } catch {
+      errorToast('ユーザー情報の更新に失敗しました')
+    }
+  }
 
   useEffect(() => {
     if (currentUser) {
@@ -38,6 +62,7 @@ export const Setting: VFC = () => {
         />
       </FormControl>
 
-    <PrimaryButton>保存する</PrimaryButton>
-  </>
-)
+      <PrimaryButton onClickButton={handleUpdateUser}>保存する</PrimaryButton>
+    </>
+  )
+}
