f188ec88fb2f8ddd7b2f471c1227996d3fab88f7
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-15 13:56:39 +0900

    招待画面のスタイルを調整

--- frontend/src/components/pages/Invitation.tsx
+++ frontend/src/components/pages/Invitation.tsx
@@ -1,6 +1,8 @@
 import { VFC } from 'react'
 import { useLocation } from 'react-router-dom'
 
+import { Box, Heading } from '@chakra-ui/react'
+
 type LocationState = {
   token: string
 }
@@ -8,10 +10,20 @@ type LocationState = {
 export const Invitation: VFC = () => {
   const location = useLocation<LocationState>()
   return (
-    <>
-      <h1>招待</h1>
-      <p>下記のURLを共有してください</p>
-      <p>{`${window.location.protocol}//${window.location.host}/signup?token=${location.state.token}`}</p>
-    </>
+    <Box p={6}>
+      <Heading size="lg" textAlign="center" my={10}>
+        一緒に使う相手を
+        <br />
+        招待しましょう
+      </Heading>
+      <Box mb={6}>
+        <p>
+          会員登録が完了しました！アプリを一緒に使いたい相手とのペアリング設定を行います。下記のURLを招待したい相手に渡してください
+        </p>
+      </Box>
+      <Box bg="gray.50" p={4}>
+        <p>{`${window.location.protocol}//${window.location.host}/signup?token=${location.state.token}`}</p>
+      </Box>
+    </Box>
   )
 }
