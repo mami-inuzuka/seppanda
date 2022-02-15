0d246f6ebe9cd0117fbccd358098fe0242d3fed2
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-15 21:07:05 +0900

    mapの各要素にkeyを設定する

--- frontend/src/components/organisms/PaymentList.tsx
+++ frontend/src/components/organisms/PaymentList.tsx
@@ -21,46 +21,45 @@ export const PaymentList: VFC<Props> = memo((props) => {
     })
 
   return (
-    <Box>
-      {paymentList.map((paymentListItem) =>
-        paymentListItem.payments.map((payment, index) => (
-          <>
-            {index === 0 && (
-              <Box bg="gray.50" fontSize="xs" color="gray.400" p={1}>
-                <p>{DateTime.fromISO(payment.paidAt).toFormat('yyyy.MM.dd')}</p>
-              </Box>
-            )}
-            <Flex
-              justify="space-between"
-              align="center"
-              key={payment.id}
-              onClick={() => handleRowClick(payment)}
-              p={4}
-              borderBottom="1px"
-              borderBottomColor="gray.50"
-            >
-              <Image
-                src={payment.avatar?.data}
-                w="28px"
-                h="28px"
-                mr={3}
-                border="2px"
-                borderColor="brand.primary"
-                borderRadius="100px"
-                overflow="hidden"
-              />
-              <Text flex="1" fontSize="sm">
-                {payment.detail}
-              </Text>
-              <Flex display="flex" align="baseline" _after={{ content: `"円"`, fontSize: 'xs' }}>
-                <Text fontSize="md" fontWeight="bold" mr="2px">
-                  {payment.amount.toLocaleString()}
+    <>
+      {paymentList.map((paymentListItem) => (
+        <Box key={paymentListItem.date}>
+          <Box bg="gray.50" fontSize="xs" color="gray.400" p={1}>
+            {DateTime.fromISO(paymentListItem.date).toFormat('yyyy.MM.dd')}
+          </Box>
+          {paymentListItem.payments.map((payment) => (
+            <Box key={payment.id}>
+              <Flex
+                justify="space-between"
+                align="center"
+                onClick={() => handleRowClick(payment)}
+                p={4}
+                borderBottom="1px"
+                borderBottomColor="gray.50"
+              >
+                <Image
+                  src={payment.avatar?.data}
+                  w="28px"
+                  h="28px"
+                  mr={3}
+                  border="2px"
+                  borderColor="brand.primary"
+                  borderRadius="100px"
+                  overflow="hidden"
+                />
+                <Text flex="1" fontSize="sm">
+                  {payment.detail}
                 </Text>
+                <Flex display="flex" align="baseline" _after={{ content: `"円"`, fontSize: 'xs' }}>
+                  <Text fontSize="md" fontWeight="bold" mr="2px">
+                    {payment.amount.toLocaleString()}
+                  </Text>
+                </Flex>
               </Flex>
-            </Flex>
-          </>
-        ))
-      )}
-    </Box>
+            </Box>
+          ))}
+        </Box>
+      ))}
+    </>
   )
 })
