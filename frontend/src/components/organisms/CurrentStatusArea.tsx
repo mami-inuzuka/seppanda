53ff6a271e8fa74e4709f04a56a082594cf1549b
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-15 14:06:17 +0900

    金額表示のスタイルを調整

--- frontend/src/components/organisms/CurrentStatusArea.tsx
+++ frontend/src/components/organisms/CurrentStatusArea.tsx
@@ -1,6 +1,6 @@
 import { memo, useContext, VFC } from 'react'
 
-import { Box, Center } from '@chakra-ui/react'
+import { Box, Center, Text } from '@chakra-ui/react'
 
 import { AuthContext } from 'context/AuthContext'
 import { PaymentContext } from 'context/PaymentContext'
@@ -10,7 +10,7 @@ export const CurrentStatusArea: VFC = memo(() => {
   const { currentUser } = useContext(AuthContext)
 
   return (
-    <Box p={16}>
+    <Box p={12}>
       {teamStatus.refundAmount === 0 && '現在貸し借りはありません'}
       {teamStatus.refundAmount !== 0 && currentUser?.id === teamStatus.largestPaymentUser?.id && (
         <Center>{teamStatus.smallestPaymentUser?.name}に返してもらう金額</Center>
@@ -18,7 +18,11 @@ export const CurrentStatusArea: VFC = memo(() => {
       {teamStatus.refundAmount !== 0 && currentUser?.id !== teamStatus.largestPaymentUser?.id && (
         <Center>{teamStatus.largestPaymentUser?.name}に返す金額</Center>
       )}
-      <Center>{teamStatus.refundAmount !== 0 && teamStatus.refundAmount}</Center>
+      <Center>
+        <Text fontSize="56px" fontWeight="bold" _after={{ content: `"円"`, fontSize: '3xl' }}>
+          {teamStatus.refundAmount !== 0 && teamStatus.refundAmount.toLocaleString()}
+        </Text>
+      </Center>
     </Box>
   )
 })
