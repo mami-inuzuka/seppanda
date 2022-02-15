1de26fb14da1cbabf88640f7db065970aaafd064
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-15 12:38:55 +0900

    Inputのスタイルを定義

---
+++ frontend/src/theme/components/Input.ts
@@ -0,0 +1,29 @@
+export const Input = {
+  sizes: {
+    lg: {
+      field: {
+        borderRadius: 'md',
+        fontSize: 'lg',
+        height: 14,
+        paddingX: 4,
+      },
+    },
+  },
+  variants: {
+    outline: {
+      field: {
+        background: 'white',
+        borderColor: 'inherit',
+        _focus: {
+          zIndex: 1,
+          borderColor: 'brand.primary',
+          boxShadow: 0,
+        },
+      },
+    },
+  },
+  defaultProps: {
+    size: 'lg',
+    variant: 'outline',
+  },
+}
