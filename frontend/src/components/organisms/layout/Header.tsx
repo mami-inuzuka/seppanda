19da8bbc58e5adaeda418b70fcc1481459b55e3c
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-16 10:12:38 +0900

    ヘッダーにロゴを設置

--- frontend/src/components/organisms/layout/Header.tsx
+++ frontend/src/components/organisms/layout/Header.tsx
@@ -2,12 +2,14 @@ import { memo, VFC } from 'react'
 import { Link } from 'react-router-dom'
 
 import { SettingsIcon } from '@chakra-ui/icons'
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Flex, Img } from '@chakra-ui/react'
+
+import logo from 'assets/images/logo-horizon.svg'
 
 export const Header: VFC = memo(() => (
   <Flex as="header" align="center" h="64px" borderBottom="1px" borderColor="gray.100" p={4}>
     <Box flex="1" textAlign="center">
-      せっぱんだ
+      <Img src={logo} />
     </Box>
     <Link to="/setting">
       <SettingsIcon w="24px" h="24px" />
