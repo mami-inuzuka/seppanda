3becaa3580f579f9732571d44657b5ea116f621a
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-16 13:32:45 +0900

    コピーアイコンをクリックして招待URLをクリップボードにコピーする

--- frontend/src/components/organisms/InvitationAlert.tsx
+++ frontend/src/components/organisms/InvitationAlert.tsx
@@ -1,7 +1,10 @@
 import { FC } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 import { CopyIcon } from '@chakra-ui/icons'
 import { Box, Flex, Text } from '@chakra-ui/react'
+
+import { useToast } from 'lib/toast'
 
 type Props = {
   invitationToken: string
@@ -9,6 +12,11 @@ type Props = {
 
 export const InvitationAlert: FC<Props> = (props) => {
   const { invitationToken } = props
+  const { successToast } = useToast()
+  const invitationUrl = `${window.location.protocol}//${window.location.host}/signup?token=${invitationToken}`
+  const handleFeedback = () => {
+    successToast('コピーしました')
+  }
   return (
     <Box bg="brand.blue" p={4} color="white">
       <Text fontSize="xs" fontWeight="bold" align="center" mb={3}>
@@ -17,10 +25,12 @@ export const InvitationAlert: FC<Props> = (props) => {
         下記のURLを共有して登録をしてもらおう
       </Text>
       <Flex bg="cyan.500" p={3}>
-        <Text fontSize="xs">{`${window.location.protocol}//${window.location.host}/signup?token=${invitationToken}`}</Text>
-        <Box>
-          <CopyIcon />
-        </Box>
+        <Text fontSize="xs">{invitationUrl}</Text>
+        <CopyToClipboard text={invitationUrl}>
+          <Box onClick={handleFeedback}>
+            <CopyIcon />
+          </Box>
+        </CopyToClipboard>
       </Flex>
     </Box>
   )
