03861fa748005eb95b5f6910417ad661499a167c
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-18 19:11:54 +0900

    currentStatusのタイトルテキストをコンポーネントに切り出す

---
+++ frontend/src/components/molcules/CardText.tsx
@@ -0,0 +1,32 @@
+import { useContext, VFC } from 'react'
+
+import { Flex, Text } from '@chakra-ui/react'
+
+import { AuthContext } from 'context/AuthContext'
+import { PaymentContext } from 'context/PaymentContext'
+
+export const CardText: VFC = () => {
+  const { currentUser } = useContext(AuthContext)
+  const { teamStatus } = useContext(PaymentContext)
+  return (
+    <Flex fontSize="sm" fontWeight="bold" justify="center" align="center" mb={2}>
+      {teamStatus.refundAmount === 0 ? (
+        <Text>現在貸し借りはありません</Text>
+      ) : (
+        <>
+          あいてに
+          {currentUser?.isDebt ? (
+            <Text bg="rgba(208, 57, 57, 0.1)" color="red.500" py={0.5} px={1.5} mx={1}>
+              返す
+            </Text>
+          ) : (
+            <Text bg="rgba(107, 178, 73, 0.1)" color="green.500" py={0.5} px={1.5} mx={1}>
+              返してもらう
+            </Text>
+          )}
+          金額
+        </>
+      )}
+    </Flex>
+  )
+}
--- frontend/src/components/organisms/CurrentStatusArea.tsx
+++ frontend/src/components/organisms/CurrentStatusArea.tsx
@@ -1,16 +1,15 @@
 import { memo, useContext, VFC } from 'react'
 
-import { Box, Center, Flex, Text, useDisclosure } from '@chakra-ui/react'
+import { Box, Center, Text, useDisclosure } from '@chakra-ui/react'
 
 import cardLabel from 'assets/images/card-label.svg'
 import { SecondaryButton } from 'components/atoms/button/SecondaryButton'
+import { CardText } from 'components/molcules/CardText'
 import { BasicModal } from 'components/organisms/BasicModal'
-import { AuthContext } from 'context/AuthContext'
 import { PaymentContext } from 'context/PaymentContext'
 
 export const CurrentStatusArea: VFC = memo(() => {
   const { teamStatus } = useContext(PaymentContext)
-  const { currentUser } = useContext(AuthContext)
   const { isOpen: isOpenSettleModal, onOpen: onOpenSettleModal, onClose: onCloseSettleModal } = useDisclosure()
 
   return (
@@ -51,25 +50,7 @@ export const CurrentStatusArea: VFC = memo(() => {
             transform: 'rotate(180deg)',
           }}
         >
-          {teamStatus.refundAmount === 0 && '現在貸し借りはありません'}
-          {teamStatus.refundAmount !== 0 && !currentUser?.isDebt && (
-            <Flex fontSize="sm" fontWeight="bold" justify="center" align="center" mb={2}>
-              あいてに
-              <Text bg="rgba(107, 178, 73, 0.1)" color="green.500" py={0.5} px={1.5} mx={1}>
-                返してもらう
-              </Text>
-              金額
-            </Flex>
-          )}
-          {teamStatus.refundAmount !== 0 && currentUser?.isDebt && (
-            <Flex fontSize="sm" fontWeight="bold" justify="center" align="center" mb={2}>
-              あいてに
-              <Text bg="rgba(208, 57, 57, 0.1)" color="red.500" py={0.5} px={1.5} mx={1}>
-                返す
-              </Text>
-              金額
-            </Flex>
-          )}
+          <CardText />
           <Center mb={4}>
             <Text fontSize="5xl" lineHeight="1" fontWeight="bold" _after={{ content: `"円"`, fontSize: '3xl' }}>
               {teamStatus.refundAmount !== 0 && teamStatus.refundAmount.toLocaleString()}
