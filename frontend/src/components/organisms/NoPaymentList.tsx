00d0568e0802c16ca6e77ef2943cb2daab59b18c
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-18 22:38:25 +0900

    NoPaymentListのスタイルを調整

--- frontend/src/components/organisms/NoPaymentList.tsx
+++ frontend/src/components/organisms/NoPaymentList.tsx
@@ -5,16 +5,8 @@ import { Flex, Image, Text } from '@chakra-ui/react'
 import logomark from 'assets/images/logomark.svg'
 
 export const NoPaymentList: VFC = () => (
-  <Flex
-    direction="column"
-    justify="center"
-    align="center"
-    borderTopColor="gray.50"
-    borderTopWidth="1px"
-    h="100%"
-    pt={12}
-  >
-    <Text fontSize="sm" align="center" mb={4} color="gray.200">
+  <Flex direction="column" justify="center" align="center" pt={12}>
+    <Text fontSize="sm" align="center" mb={4} color="gray.300">
       支払い情報はまだありません
     </Text>
     <Image src={logomark} w="60px" opacity="0.1" />
