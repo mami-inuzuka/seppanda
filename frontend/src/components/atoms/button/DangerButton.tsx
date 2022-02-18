9c33c3904141f666a52acc8b963838e23e742095
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-18 20:05:01 +0900

    DangerButtonにsize/onClick/isFullWidthプロパティを追加

--- frontend/src/components/atoms/button/DangerButton.tsx
+++ frontend/src/components/atoms/button/DangerButton.tsx
@@ -4,12 +4,24 @@ import { Button } from '@chakra-ui/react'
 
 type Props = {
   children: React.ReactNode
+  isFullWidth: boolean
+  size: string
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => Promise<void> | (() => void)
 }
 
 export const DangerButton: VFC<Props> = memo((props) => {
-  const { children } = props
+  const { children, size, isFullWidth, onClick } = props
   return (
-    <Button bgColor="gray.50" borderRadius={4} border="1px" borderColor="gray.200" color="red.500">
+    <Button
+      bgColor="gray.50"
+      borderRadius={4}
+      border="1px"
+      borderColor="gray.200"
+      color="red.500"
+      onClick={onClick}
+      size={size}
+      isFullWidth={isFullWidth}
+    >
       {children}
     </Button>
   )
