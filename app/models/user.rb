ac71d8cdb14b0af189e06ace2aab15b905a12efd
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-21 23:38:09 +0900

    deviseのモジュールを削除しFirebase Authenticationのモジュールを読み込む

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,4 @@ class User < ApplicationRecord
   has_one_attached :avatar
   belongs_to :team
   has_many :payments, dependent: :destroy
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-  include DeviseTokenAuth::Concerns::User
-  validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-      registrations: 'overrides/registrations',
-      confirmations: 'overrides/confirmations'
-    }
-
     namespace :auth do
       resources :sessions, only: %i[index]
     end
