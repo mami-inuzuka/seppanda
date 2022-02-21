82f5daca45c454c0a6dfed9122eab8342b8ec382
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-21 14:33:53 +0900

    招待URLをアイコンをクリックしてコピーできるようにする

--- frontend/src/components/pages/Invitation.tsx
+++ frontend/src/components/pages/Invitation.tsx
@@ -1,15 +1,23 @@
 import { VFC } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { useLocation } from 'react-router-dom'
 
-import { Box, Heading } from '@chakra-ui/react'
+import { CopyIcon } from '@chakra-ui/icons'
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
 
 import { HeaderOnlyLogoLayout } from 'components/templates/HeaderOnlyLogoLayout'
+import { useToast } from 'lib/toast'
 
 export const Invitation: VFC = () => {
   const { search } = useLocation()
+  const { successToast } = useToast()
 
   const query = new URLSearchParams(search)
   const invitationToken = query.get('invitation_token')
+  const invitationUrl = `${window.location.protocol}//${window.location.host}/signup?token=${invitationToken}`
+  const handleFeedback = () => {
+    successToast('コピーしました')
+  }
   return (
     <HeaderOnlyLogoLayout>
       <Box p={6}>
@@ -20,12 +28,18 @@ export const Invitation: VFC = () => {
         </Heading>
         <Box mb={6}>
           <p>
-            会員登録が完了しました！アプリを一緒に使いたい相手とのペアリング設定を行います。下記のURLを招待したい相手に渡してください
+            会員登録が完了しました！アプリを一緒に使いたい相手とのペアリング設定を行います。下記のURLを招待したい相手に共有してください
           </p>
         </Box>
-        <Box bg="gray.50" p={4}>
-          <p>{`${window.location.protocol}//${window.location.host}/signup?token=${invitationToken}`}</p>
-        </Box>
+
+        <Flex bg="gray.100" p={4} align="center">
+          <Text>{invitationUrl}</Text>
+          <CopyToClipboard text={invitationUrl}>
+            <Box onClick={handleFeedback}>
+              <CopyIcon />
+            </Box>
+          </CopyToClipboard>
+        </Flex>
       </Box>
     </HeaderOnlyLogoLayout>
   )
