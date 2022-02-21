9af3a1ee3b4f7c3439ed5388a8483a86d10f418e
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-21 23:38:44 +0900

    firebaseの初期化ファイルを作成

---
+++ frontend/src/lib/firebase.ts
@@ -0,0 +1,14 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+}
+firebase.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
