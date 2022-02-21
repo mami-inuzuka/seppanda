5ff509f96b8900e1e127478c58f0ec773e19fbdf
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-21 18:03:15 +0900

    production環境にaction_mailer.default_url_optionsを設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: ENV['HOST']}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   port: 587,
