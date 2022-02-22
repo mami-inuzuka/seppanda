2079c9c3fb5b7078a7cc35e39e735ddb98652434
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-22 19:21:39 +0900

    SignInページにGoogleログインを設置

--- frontend/src/components/pages/SignIn.tsx
+++ frontend/src/components/pages/SignIn.tsx
@@ -1,76 +1,57 @@
-import { useState, useContext, VFC, memo } from 'react'
-import { useHistory } from 'react-router-dom'
+import { VFC, memo, useContext } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
 
-import { Box, FormControl, FormLabel, Grid, Heading, Input } from '@chakra-ui/react'
+import { Box, Button, Heading, Image } from '@chakra-ui/react'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 
-import { PrimaryButton } from 'components/atoms/button/PrimaryButton'
+import googleIcon from 'assets/images/google_icon.svg'
 import { HeaderOnlyLogoLayout } from 'components/templates/HeaderOnlyLogoLayout'
 import { AuthContext } from 'context/AuthContext'
-import { getCurrentUser } from 'lib/api/auth'
-import { auth } from 'lib/firebase'
+import { signUp } from 'lib/api/auth'
 
 export const SignIn: VFC = memo(() => {
-  // 関連issue: [react-router-dom] - Export History type #50526 https://github.com/DefinitelyTyped/DefinitelyTyped/issues/50526
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  const { setIsSignedIn, setCurrentUser } = useContext(AuthContext)
   const history = useHistory()
-  const { setCurrentUser, setIsLoaded } = useContext(AuthContext)
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
+  const { search } = useLocation()
+  const query = new URLSearchParams(search)
+  const invitationToken = query.get('token')
 
-  const handleGetCurrentUser = async () => {
-    console.log('SIgnIn:handleGetCurrentUser')
-    try {
-      const idToken = await auth.currentUser?.getIdToken(true)
-      const uid = auth.currentUser?.uid
-      if (uid) {
-        const res = await getCurrentUser({ uid }, idToken)
-        if (res?.status === 200) {
-          setCurrentUser(res.data.user)
-          setIsLoaded(true)
-        }
-      }
-    } catch (e) {
-      console.log(e)
+  const handleCreateUser = async () => {
+    const token = await auth.currentUser?.getIdToken(true)
+    const data = { token }
+    const res = await signUp(data, invitationToken)
+    if (res.status === 200) {
+      setCurrentUser(res.data?.user)
+      setIsSignedIn(true)
     }
   }
 
-  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    // デフォルトの動作をキャンセル
-    e.preventDefault()
-    // eslint-disable-next-line no-debugger
-    debugger
-    await auth.signInWithEmailAndPassword(email, password).then(handleGetCurrentUser)
+  const auth = getAuth()
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(auth, provider)
+      .then(handleCreateUser)
+      .then(() => {
+        history.push({
+          pathname: '/invitation',
+          state: '',
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
   return (
     <HeaderOnlyLogoLayout>
       <Box p={6}>
         <Heading size="lg" textAlign="center" my={10}>
-          ログイン
+          はじめる
         </Heading>
-        <form noValidate autoComplete="off">
-          <Grid gap={6}>
-            <FormControl>
-              <FormLabel htmlFor="email">メールアドレス</FormLabel>
-              <Input value={email} placeholder="メールアドレス" onChange={(event) => setEmail(event.target.value)} />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="password">パスワード</FormLabel>
-              <Input
-                value={password}
-                placeholder="パスワード"
-                onChange={(event) => setPassword(event.target.value)}
-                type="password"
-              />
-            </FormControl>
-            <PrimaryButton
-              disabled={!!(!email || !password)} // 空欄があった場合はボタンを押せないように
-              onClickButton={handleSubmit}
-            >
-              ログイン
-            </PrimaryButton>
-          </Grid>
-        </form>
+        <Button onClick={signInWithGoogle} bg="gray.100" size="xl" isFullWidth>
+          <Image src={googleIcon} mr={2} />
+          Googleアカウントではじめる
+        </Button>
       </Box>
     </HeaderOnlyLogoLayout>
   )
