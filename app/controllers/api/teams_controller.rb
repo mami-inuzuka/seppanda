d0d5ae6dd6ff6538c751e955bd5090d6958a1e80
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-22 13:40:46 +0900

    firebase-auth-railsの仕様に合わせてcurrent_userの記述やbefore_actionを修正

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class Api::TeamsController < ApplicationController
-  before_action :authenticate_api_user!
-
   def show
-    @team = Team.find(current_api_user.team_id)
+    @team = Team.find(current_user.team_id)
     render :show
   end
 end
