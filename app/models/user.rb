10b63159394235110322150c42bf3b39d5d5bb05
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-23 12:04:37 +0900

    Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   has_one_attached :avatar
   belongs_to :team
   has_many :payments, dependent: :destroy
+  validates :email, :uid, uniqueness: true
+  validates :email, :uid, presence: true
 end
