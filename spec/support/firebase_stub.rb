ae3d95ed3c5e249ffe1cd04f31865b4aa938eb33
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-23 15:12:53 +0900

    テストでFirebaseのJWT認証を通すためのスタブを作成

---
+++ spec/support/firebase_stub.rb
@@ -0,0 +1,14 @@
+module FirebaseStub
+  def stub_firebase(user)
+    allow_any_instance_of(Firebase::Auth::Authenticable).to receive(:authenticate_entity).and_return(user)
+  end
+
+  def stub_authenticate_new_user
+    user_info = {
+      'name' => 'Alice',
+      'email' => 'alice@example.com',
+      'sub' => '12345'
+    }
+    allow_any_instance_of(Api::Auth::RegistrationsController).to receive(:payload).and_return(user_info)
+  end
+end
