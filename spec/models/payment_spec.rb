7d442bbc1808d7ac0cbce63c6baa8ac27b1122be
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-26 14:47:39 +0900

    amountカラムとdetailカラムに分けてテストを記述する

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -4,98 +4,81 @@ require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
   let(:user) { create(:user, :with_team) }
+  let(:team_id) { user.team_id }
 
-  example '0以上9999999以下の半角数字で整数であれば登録できる' do
-    payment = Payment.new(amount: 100)
-    payment.user_id = user.id
-    payment.team_id = user.team_id
-    payment.valid?
-    expect(payment).to be_valid
+  context 'amount' do
+    example '0以上9999999以下の半角数字で整数であれば登録できる' do
+      payment = user.payments.build(amount: 100, team_id: team_id, paid_at: '2022-02-14')
+      payment.valid?
+      expect(payment).to be_valid
+    end
+
+    example '全角数字は登録できない' do
+      payment = user.payments.build(amount: '１００', team_id: team_id, paid_at: '2022-02-14')
+      payment.valid?
+      expect(payment).to be_invalid
+      expect(payment.errors[:amount]).to include('is not a number')
+    end
+
+    example '金額の入力がないと登録できない' do
+      payment = user.payments.build(amount: nil, team_id: team_id, paid_at: '2022-02-14')
+      payment.valid?
+      expect(payment).to be_invalid
+      expect(payment.errors[:amount]).to include("can't be blank")
+    end
+
+    example '小数は登録できない' do
+      payment = user.payments.build(amount: 100.5, team_id: team_id, paid_at: '2022-02-14')
+      payment.valid?
+      expect(payment).to be_invalid
+      expect(payment.errors[:amount]).to include('must be an integer')
+    end
+
+    example '0は登録できない' do
+      payment = user.payments.build(amount: 0, team_id: team_id, paid_at: '2022-02-14')
+      payment.valid?
+      expect(payment).to be_invalid
+      expect(payment.errors[:amount]).to include('must be greater than 0')
+    end
+
+    example '1は登録できる' do
+      payment = user.payments.build(amount: 1, team_id: team_id, paid_at: '2022-02-14')
+      payment.valid?
+      expect(payment).to be_valid
+    end
+
+    example '9999999は登録できる' do
+      payment = user.payments.build(amount: 9999999, team_id: team_id, paid_at: '2022-02-14')
+      payment.valid?
+      expect(payment).to be_valid
+    end
+
+    example '10000000は登録できない' do
+      payment = user.payments.build(amount: 10000000, team_id: team_id, paid_at: '2022-02-14')
+      payment.valid?
+      expect(payment).to be_invalid
+      expect(payment.errors[:amount]).to include('must be less than or equal to 9999999')
+    end
   end
 
-  example '全角数字は登録できない' do
-    payment = Payment.new(amount: '１００')
-    payment.user_id = user.id
-    payment.team_id = user.team_id
-    payment.valid?
-    expect(payment).to be_invalid
-    expect(payment.errors[:amount]).to include('is not a number')
-  end
+  context 'detail' do
+    example '0文字でも登録できる' do
+      payment = user.payments.build(amount: 100, detail: '', team_id: team_id, paid_at: '2022-02-14')
+      payment.valid?
+      expect(payment).to be_valid
+    end
 
-  example '金額の入力がないと登録できない' do
-    payment = Payment.new(amount: nil)
-    payment.user_id = user.id
-    payment.team_id = user.team_id
-    payment.valid?
-    expect(payment).to be_invalid
-    expect(payment.errors[:amount]).to include("can't be blank")
-  end
+    example '28文字だと登録できる' do
+      payment = user.payments.build(amount: 100, detail: 'ああああああああああああああああああああああああああああ', team_id: team_id, paid_at: '2022-02-14')
+      payment.valid?
+      expect(payment).to be_valid
+    end
 
-  example '小数は登録できない' do
-    payment = Payment.new(amount: 100.5)
-    payment.user_id = user.id
-    payment.team_id = user.team_id
-    payment.valid?
-    expect(payment).to be_invalid
-    expect(payment.errors[:amount]).to include('must be an integer')
-  end
-
-  example '0は登録できない' do
-    payment = Payment.new(amount: 0)
-    payment.user_id = user.id
-    payment.team_id = user.team_id
-    payment.valid?
-    expect(payment).to be_invalid
-    expect(payment.errors[:amount]).to include('must be greater than 0')
-  end
-
-  example '1は登録できる' do
-    payment = Payment.new(amount: 1)
-    payment.user_id = user.id
-    payment.team_id = user.team_id
-    payment.valid?
-    expect(payment).to be_valid
-  end
-
-  example '9999999は登録できる' do
-    payment = Payment.new(amount: 9999999)
-    payment.user_id = user.id
-    payment.team_id = user.team_id
-    payment.valid?
-    expect(payment).to be_valid
-  end
-
-  example '10000000は登録できない' do
-    payment = Payment.new(amount: 10000000)
-    payment.user_id = user.id
-    payment.team_id = user.team_id
-    payment.valid?
-    expect(payment).to be_invalid
-    expect(payment.errors[:amount]).to include('must be less than or equal to 9999999')
-  end
-
-  example 'detailは0文字でも登録できる' do
-    payment = Payment.new(amount: 100, detail: '')
-    payment.user_id = user.id
-    payment.team_id = user.team_id
-    payment.valid?
-    expect(payment).to be_valid
-  end
-
-  example 'detailが28文字だと登録できる' do
-    payment = Payment.new(amount: 100, detail: 'ああああああああああああああああああああああああああああ')
-    payment.user_id = user.id
-    payment.team_id = user.team_id
-    payment.valid?
-    expect(payment).to be_valid
-  end
-
-  example 'detailが29文字だと登録できない' do
-    payment = Payment.new(amount: 100, detail: 'あああああああああああああああああああああああああああああ')
-    payment.user_id = user.id
-    payment.team_id = user.team_id
-    payment.valid?
-    expect(payment).to be_invalid
-    expect(payment.errors[:detail]).to include('is too long (maximum is 28 characters)')
+    example '29文字だと登録できない' do
+      payment = user.payments.build(amount: 100, detail: 'あああああああああああああああああああああああああああああ', team_id: team_id, paid_at: '2022-02-14')
+      payment.valid?
+      expect(payment).to be_invalid
+      expect(payment.errors[:detail]).to include('is too long (maximum is 28 characters)')
+    end
   end
 end
