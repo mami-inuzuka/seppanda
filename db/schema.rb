2b7ca41e05c4f858ff95fae97037159ab21d6202
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-27 13:45:08 +0900

    uidカラムのdefault値をなくす

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_27_043754) do
+ActiveRecord::Schema.define(version: 2022_02_27_044322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2022_02_27_043754) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "uid", default: "", null: false
+    t.string "uid", null: false
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
