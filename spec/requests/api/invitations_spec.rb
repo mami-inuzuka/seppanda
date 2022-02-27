b7adaa895beec447dcb745cfab8562de483595c9
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-27 22:10:36 +0900

    Invitationsコントローラーのリクエストspecを作成

---
+++ spec/requests/api/invitations_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Api::Invitations', type: :request do
+  let(:user) { create(:user, :with_team) }
+  let(:invitation_token) { Team.find(user.team_id).invitation_token }
+
+  describe 'GET /api/invitations' do
+    example '正しいinvitation_tokenの場合招待者情報が返る' do
+      get api_invitations_path, headers: { invitationToken: invitation_token }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)['name']).to be_present
+      expect(JSON.parse(response.body)['avatar']['data']).to be_present
+      expect(JSON.parse(response.body)['avatar']['name']).to be_present
+    end
+
+    example '不正なinvitation_tokenの場合404が返る' do
+      get api_invitations_path, headers: { invitationToken: '12345' }
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+end
