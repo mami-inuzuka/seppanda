0bae79edef8ce39bd164ac703e760148e139e72b
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-28 08:40:07 +0900

    devise_token_authに関する記述を削除

--- app/controllers/api/teams/payments_controller.rb
+++ app/controllers/api/teams/payments_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Api::Teams::PaymentsController < Api::ApplicationController
-  before_action :authenticate_api_user!
-
   def update
     payments = Payment.includes(:user).where(team_id: current_user.team_id, settled: false)
     payments.update_all(settled: true, settled_at: Time.zone.now) # rubocop:disable Rails/SkipsModelValidations
