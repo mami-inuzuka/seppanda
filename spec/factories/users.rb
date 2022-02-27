5d3a82140381e1e14770f7efd438db468d817a02
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-27 22:05:46 +0900

    Factory Botでuserをbuildした際にActive Storageの画像を紐づける

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,5 +8,8 @@ FactoryBot.define do
     trait :with_team do
       association :team
     end
+    after(:build) do |user|
+      user.avatar.attach(io: File.open('spec/fixtures/files/test_user_icon.png'), filename: 'test_user_icon.png', content_type: 'image/png')
+    end
   end
 end
