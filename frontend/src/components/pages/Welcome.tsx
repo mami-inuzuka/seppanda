88232b8c582ece5183bec17a2deae8fb370ede0d
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-28 09:19:16 +0900

    Googleログインをした後次のページに遷移するまでspinnerを表示する

--- frontend/src/components/pages/Welcome.tsx
+++ frontend/src/components/pages/Welcome.tsx
@@ -1,7 +1,7 @@
-import { VFC, memo } from 'react'
+import { VFC, memo, useState } from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
 
-import { Box, Button, Flex, Image, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Image, Spinner, Text } from '@chakra-ui/react'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 
 import googleIcon from 'assets/images/google_icon.svg'
@@ -9,12 +9,14 @@ import LogoWithCopy from 'assets/images/logo-with-copy.svg'
 import { getCurrentUser } from 'lib/api/session'
 
 export const Welcome: VFC = memo(() => {
+  const [isLoading, setIsLoading] = useState(false)
   const history = useHistory()
   const { search } = useLocation()
   const query = new URLSearchParams(search)
   const invitationToken = query.get('invitation_token')
 
   const handleGetCurrentUser = async () => {
+    setIsLoading(true)
     const token = await auth.currentUser?.getIdToken(true)
     const res = await getCurrentUser(token)
     return res
@@ -43,17 +45,36 @@ export const Welcome: VFC = memo(() => {
   }
 
   return (
-    <Flex h="100vh" justify="center" align="center" p={6}>
-      <Box>
-        <Image src={LogoWithCopy} margin="0 auto" mb="14" />
-        <Button onClick={signInWithGoogle} bg="gray.100" size="xl" mb={6} isFullWidth>
-          <Image src={googleIcon} mr={2} />
-          Googleでログインする
-        </Button>
-        <Text fontSize="xs" align="center" color="gray.400">
-          上記のボタンをクリックすることで、利用規約およびプライバシーポリシーに同意するものとします。
-        </Text>
-      </Box>
-    </Flex>
+    <>
+      {isLoading && (
+        <Flex
+          w="100%"
+          h="100vh"
+          direction="column"
+          justify="center"
+          align="center"
+          p={6}
+          position="fixed"
+          top="0"
+          left="0"
+          bg="rgba(255,255,255,0.7)"
+          zIndex="1"
+        >
+          <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="green.500" size="xl" />
+        </Flex>
+      )}
+      <Flex h="100vh" justify="center" align="center" p={6}>
+        <Box>
+          <Image src={LogoWithCopy} margin="0 auto" mb="14" />
+          <Button onClick={signInWithGoogle} bg="gray.100" size="xl" mb={6} isFullWidth disabled={isLoading}>
+            <Image src={googleIcon} mr={2} />
+            Googleでログインする
+          </Button>
+          <Text fontSize="xs" align="center" color="gray.400">
+            上記のボタンをクリックすることで、利用規約およびプライバシーポリシーに同意するものとします。
+          </Text>
+        </Box>
+      </Flex>
+    </>
   )
 })
