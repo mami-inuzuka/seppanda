a48bd76e2a158239f8658fb032b8ec58b2f7b945
mami-inuzuka <inuzuka.mami@gmail.com>
2022-02-28 15:44:23 +0900

    api/teams/payments#updateに正しくエラーメッセージとstatusを設定する

--- app/controllers/api/teams/payments_controller.rb
+++ app/controllers/api/teams/payments_controller.rb
@@ -3,6 +3,10 @@
 class Api::Teams::PaymentsController < Api::ApplicationController
   def update
     payments = Payment.includes(:user).where(team_id: current_user.team_id, settled: false)
-    payments.update_all(settled: true, settled_at: Time.zone.now) # rubocop:disable Rails/SkipsModelValidations
+    if payments.present?
+      payments.update_all(settled: true, settled_at: Time.zone.now) # rubocop:disable Rails/SkipsModelValidations
+    else
+      render json: { message: '支払情報がないため精算処理を実行できません' }, status: :unprocessable_entity
+    end
   end
 end
