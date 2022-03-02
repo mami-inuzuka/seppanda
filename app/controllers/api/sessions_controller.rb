ad0ed557a0087da46795a593f54176232e9187c6
mami-inuzuka <inuzuka.mami@gmail.com>
2022-03-02 14:27:10 +0900

    payloadがnilとなってしまう場合のためにエラーを発生させる

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,6 +4,8 @@ class Api::SessionsController < Api::Auth::FirebaseAuthRailsController
   skip_before_action :authenticate_user
 
   def index
+    raise ArgumentError, 'BadRequest Parameter' if payload.blank?
+
     @user = User.find_by(uid: payload['sub'])
     render :index
   end
