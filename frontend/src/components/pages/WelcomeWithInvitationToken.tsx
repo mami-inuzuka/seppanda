8ff586f2ca5eaab1095c5925ecb95d757eb94c91
mami-inuzuka <inuzuka.mami@gmail.com>
2022-03-02 14:47:47 +0900

    signInWithGoogleが完了するまでスピナーを表示する

--- frontend/src/components/pages/WelcomeWithInvitationToken.tsx
+++ frontend/src/components/pages/WelcomeWithInvitationToken.tsx
@@ -1,7 +1,7 @@
 import { VFC, memo, useEffect, useState } from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
 
-import { Box, Button, Heading, Image, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, Image, Spinner, Text } from '@chakra-ui/react'
 import axios from 'axios'
 
 import googleIcon from 'assets/images/google_icon.svg'
@@ -51,63 +51,82 @@ export const WelcomeWithInvitationToken: VFC = memo(() => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return (
-    <Box>
-      {isLoaded && (
-        <HeaderOnlyLogoLayout>
-          <Box h="100vh" p={6}>
-            <Box mb={20}>
-              <Heading size="lg" textAlign="center" my={4}>
-                seppandaに参加する
-              </Heading>
-            </Box>
-            <Box bg="gray.100" position="relative" p={6} pt="56px">
-              <Box
-                position="absolute"
-                w="64px"
-                h="64px"
-                top="-32px"
-                left="0"
-                right="0"
-                m="auto"
-                border="2px"
-                borderColor="blue.500"
-                borderRadius="9999px"
-                overflow="hidden"
-              >
-                <Image src={inviterAvatar.data} />
-              </Box>
-              <Text align="center" fontSize="sm" lineHeight="1.8" mb={6}>
-                <Text as="span" fontWeight="bold">
-                  {inviterName}
-                </Text>
-                さんが
-                <br />
-                あなたをseppandaの利用に
-                <br />
-                招待しています。
-                <br />
-                下記のボタンから参加しましょう
-              </Text>
-
-              <Button
-                onClick={signInWithGoogle}
-                bg="white"
-                size="xl"
-                mb={6}
-                border="1px solid rgba(46, 47, 46, 0.1)"
-                boxShadow="0px 1px 0px #D7D7D7"
-                isFullWidth
-              >
-                <Image src={googleIcon} mr="24px" />
-                Googleでログインする
-              </Button>
-              <Text fontSize="xs" align="center" color="gray.400">
-                上記のボタンをクリックすることで、利用規約およびプライバシーポリシーに同意するものとします。
-              </Text>
-            </Box>
-          </Box>
-        </HeaderOnlyLogoLayout>
+    <>
+      {isLoading && (
+        <Flex
+          w="100%"
+          h="100vh"
+          direction="column"
+          justify="center"
+          align="center"
+          p={6}
+          position="fixed"
+          top="0"
+          left="0"
+          bg="rgba(255,255,255,0.7)"
+          zIndex="1"
+        >
+          <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="green.500" size="xl" />
+        </Flex>
       )}
-    </Box>
+      <Box>
+        {isLoaded && (
+          <HeaderOnlyLogoLayout>
+            <Box h="100vh" p={6}>
+              <Box mb={20}>
+                <Heading size="lg" textAlign="center" my={4}>
+                  seppandaに参加する
+                </Heading>
+              </Box>
+              <Box bg="gray.100" position="relative" p={6} pt="56px">
+                <Box
+                  position="absolute"
+                  w="64px"
+                  h="64px"
+                  top="-32px"
+                  left="0"
+                  right="0"
+                  m="auto"
+                  border="2px"
+                  borderColor="blue.500"
+                  borderRadius="9999px"
+                  overflow="hidden"
+                >
+                  <Image src={inviterAvatar.data} />
+                </Box>
+                <Text align="center" fontSize="sm" lineHeight="1.8" mb={6}>
+                  <Text as="span" fontWeight="bold">
+                    {inviterName}
+                  </Text>
+                  さんが
+                  <br />
+                  あなたをseppandaの利用に
+                  <br />
+                  招待しています。
+                  <br />
+                  下記のボタンから参加しましょう
+                </Text>
+
+                <Button
+                  onClick={signInWithGoogle}
+                  bg="white"
+                  size="xl"
+                  mb={6}
+                  border="1px solid rgba(46, 47, 46, 0.1)"
+                  boxShadow="0px 1px 0px #D7D7D7"
+                  isFullWidth
+                >
+                  <Image src={googleIcon} mr="24px" />
+                  Googleでログインする
+                </Button>
+                <Text fontSize="xs" align="center" color="gray.400">
+                  上記のボタンをクリックすることで、利用規約およびプライバシーポリシーに同意するものとします。
+                </Text>
+              </Box>
+            </Box>
+          </HeaderOnlyLogoLayout>
+        )}
+      </Box>
+    </>
   )
 })
