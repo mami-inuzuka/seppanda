3a37134916c6ac20320d8a4cb0b22395461f421d
mami-inuzuka <inuzuka.mami@gmail.com>
2022-03-03 21:35:34 +0900

    スタイルと文言を調整

--- frontend/src/components/pages/NotFound.tsx
+++ frontend/src/components/pages/NotFound.tsx
@@ -1,10 +1,11 @@
 import { VFC, memo } from 'react'
 import { useHistory } from 'react-router-dom'
 
-import { Flex, Image, Text } from '@chakra-ui/react'
+import { Box, Heading, Image, Text } from '@chakra-ui/react'
 
 import logomark from 'assets/images/logomark.svg'
 import { SecondaryButton } from 'components/atoms/button/SecondaryButton'
+import { HeaderOnlyLogoLayout } from 'components/templates/HeaderOnlyLogoLayout'
 
 export const NotFound: VFC = memo(() => {
   const history = useHistory()
@@ -14,14 +15,23 @@ export const NotFound: VFC = memo(() => {
   }
 
   return (
-    <Flex h="100vh" direction="column" justify="center" align="center" pt={12}>
-      <Text fontSize="sm" align="center" mb={4}>
-        ページが見つかりません
-      </Text>
-      <SecondaryButton onClick={handleVisitHome} size="xl" isFullWidth>
-        TOPに戻る
-      </SecondaryButton>
-      <Image src={logomark} w="60px" />
-    </Flex>
+    <HeaderOnlyLogoLayout>
+      <Box p={6}>
+        <Box my={8}>
+          <Image src={logomark} w="60px" margin="0 auto" mb={6} />
+          <Heading size="lg" textAlign="center" my={4}>
+            ページが見つかりません
+          </Heading>
+        </Box>
+        <Text fontSize="sm" align="center" mb={10}>
+          アクセスしようとしたページは削除、変更されたか現在利用できない可能性があります。
+          <br />
+          URLにタイプミスがないかご確認ください。
+        </Text>
+        <SecondaryButton onClick={handleVisitHome} size="xl" isFullWidth>
+          メインページに戻る
+        </SecondaryButton>
+      </Box>
+    </HeaderOnlyLogoLayout>
   )
 })
