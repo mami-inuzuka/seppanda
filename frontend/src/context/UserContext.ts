1a29e71510bd06f65408a2af7e581aeadc20495a
mami-inuzuka <inuzuka.mami@gmail.com>
2022-03-10 20:52:19 +0900

    UserContextとUserProviderから不要なstateを削除

--- frontend/src/context/UserContext.ts
+++ frontend/src/context/UserContext.ts
@@ -1,26 +1,14 @@
 import { createContext } from 'react'
 
-import firebase from 'firebase/compat/app'
-
 import type { User } from 'types/user'
 
 export type UserContextType = {
-  isLoaded: boolean | null
-  setIsLoaded: React.Dispatch<React.SetStateAction<boolean>>
   currentUser: User | null
   setCurrentUser: React.Dispatch<React.SetStateAction<User | null>>
-  currentFirebaseUser: firebase.User | null
-  setCurrentFirebaseUser: React.Dispatch<React.SetStateAction<firebase.User | null>>
 }
 
 export const UserContext = createContext<UserContextType>({
-  isLoaded: null,
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  setIsLoaded: () => {},
   currentUser: null,
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   setCurrentUser: () => {},
-  currentFirebaseUser: null,
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  setCurrentFirebaseUser: () => {},
 })
--- frontend/src/providers/UserProvider.tsx
+++ frontend/src/providers/UserProvider.tsx
@@ -2,30 +2,18 @@ import { useState } from 'react'
 
 import firebase from 'firebase/compat/app'
 
-import { Loading } from 'components/pages/Loading'
 import { UserContext } from 'context/UserContext'
 
 import type { User } from 'types/user'
 
 export const UserProvider = ({ children }: { children: React.ReactElement }) => {
-  const [isLoaded, setIsLoaded] = useState<boolean>(false)
   const [currentUser, setCurrentUser] = useState<User | null>(null)
-  const [currentFirebaseUser, setCurrentFirebaseUser] = useState<firebase.User | null>(null)
 
   // eslint-disable-next-line react/jsx-no-constructed-context-values
   const value = {
-    isLoaded,
-    setIsLoaded,
     currentUser,
     setCurrentUser,
-    currentFirebaseUser,
-    setCurrentFirebaseUser,
   }
 
-  return (
-    <UserContext.Provider value={value}>
-      {!isLoaded && <Loading />}
-      {isLoaded && children}
-    </UserContext.Provider>
-  )
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
