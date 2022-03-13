819c40150d7c8c7304c65ad5e39679b963462332
mami-inuzuka <inuzuka.mami@gmail.com>
2022-03-13 10:04:22 +0900

    ProviderはApp.tsxに一つずつ読み込む

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,24 @@
 import { VFC } from 'react'
 
-import { Box } from '@chakra-ui/react'
+import { Box, ChakraProvider } from '@chakra-ui/react'
 
-import { RouterConfig } from 'components/router/RouterConfig'
-import { Providers } from 'providers/Providers'
+import { AuthProvider } from 'providers/AuthProvider'
+import { PaymentProvider } from 'providers/PaymentProvider'
+import { RouterConfig } from 'router/RouterConfig'
+import { theme } from 'theme/index'
 
 const App: VFC = () => (
-  <Providers>
-    <Box w="100%" minH="100vh" bg="gray.50">
-      <Box w="100%" m="0 auto" bg="white" maxW={{ base: 'auto', sm: '400px' }} minW="320px" minH="100vh">
-        <RouterConfig />
-      </Box>
-    </Box>
-  </Providers>
+  <ChakraProvider theme={theme}>
+    <AuthProvider>
+      <PaymentProvider>
+        <Box w="100%" minH="100vh" bg="gray.50">
+          <Box w="100%" m="0 auto" bg="white" maxW={{ base: 'auto', sm: '400px' }} minW="320px" minH="100vh">
+            <RouterConfig />
+          </Box>
+        </Box>
+      </PaymentProvider>
+    </AuthProvider>
+  </ChakraProvider>
 )
 
 export default App
