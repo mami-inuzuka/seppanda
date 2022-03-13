dcadf78bf1350e95ea6f7cb32115d88902c3d13a
mami-inuzuka <inuzuka.mami@gmail.com>
2022-03-13 12:13:30 +0900

    使用不可能なuserAgentの場合ポップアップを表示する

---
+++ frontend/src/components/organisms/modal/BrowserCheckModal.tsx
@@ -0,0 +1,33 @@
+import { memo, VFC } from 'react'
+
+import { Modal, ModalHeader, ModalBody, ModalContent, ModalOverlay, Text, Box } from '@chakra-ui/react'
+
+type Props = {
+  isOpen: boolean
+  onClose: () => void
+  size: string
+}
+
+export const BrowserCheckModal: VFC<Props> = memo((props) => {
+  const { isOpen, onClose, size } = props
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size={size} closeOnOverlayClick={false} isCentered>
+      <ModalOverlay />
+      <ModalContent m={6} py={14} px={6}>
+        <ModalHeader align="center" mb={4}>
+          外部ブラウザで
+          <br />
+          開いてください
+        </ModalHeader>
+        <ModalBody p={0}>
+          <Box mb={12}>
+            <Text align="center" fontSize="sm" mb={6}>
+              LINEやInstagram、Messangerなどのアプリ内ブラウザでは正しく動作しない場合があります。お手数ですが外部ブラウザで開き直してください。
+            </Text>
+          </Box>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  )
+})
--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,53 +1,65 @@
 import { VFC } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
-import { Box, Flex, Image, Text } from '@chakra-ui/react'
+import { Box, Flex, Image, Text, useDisclosure } from '@chakra-ui/react'
 
 import VerticalLogo from 'assets/images/logo-vertical-black.svg'
 import { GoogleLoginButton } from 'components/atoms/button/GoogleLoginButton'
 import { Footer } from 'components/organisms/footer/Footer'
+import { BrowserCheckModal } from 'components/organisms/modal/BrowserCheckModal'
+import { detectInAppBrowser } from 'lib/detectInAppBrowser'
 
 export const Welcome: VFC = () => {
   const navigate = useNavigate()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const userAgent = detectInAppBrowser(window.navigator.userAgent)
+
+  const handleGoToSignInPage = () => {
+    if (userAgent) {
+      if (userAgent.match(/line|instagram|facebook/)) {
+        onOpen()
+      }
+    } else {
+      navigate('/signin', { state: { referrer: 'signin' } })
+    }
+  }
 
   return (
-    <Flex h="100%" direction="column" minH="100vh">
-      <Flex h="100%" justify="center" align="center" direction="column" p={6} flex="1" my={28}>
-        <Box>
-          <Box mb="10">
-            <Image src={VerticalLogo} margin="0 auto" mb={4} h="140px" />
-            <Text fontWeight="bold" textAlign="center" letterSpacing="0.1em">
-              毎日の
-              <Text as="span" color="green.500">
-                割り勘
+    <>
+      <BrowserCheckModal isOpen={isOpen} onClose={onClose} size="xl" />
+      <Flex h="100%" direction="column" minH="100vh">
+        <Flex h="100%" justify="center" align="center" direction="column" p={6} flex="1" my={28}>
+          <Box>
+            <Box mb="10">
+              <Image src={VerticalLogo} margin="0 auto" mb={4} h="140px" />
+              <Text fontWeight="bold" textAlign="center" letterSpacing="0.1em">
+                毎日の
+                <Text as="span" color="green.500">
+                  割り勘
+                </Text>
+                を
+                <Text as="span" color="orange.500">
+                  まとめて
+                </Text>
+                計算
               </Text>
-              を
-              <Text as="span" color="orange.500">
-                まとめて
+            </Box>
+            <GoogleLoginButton onClick={handleGoToSignInPage} disabled={false} />
+            <Text fontSize="xs" align="center" color="gray.400" lineHeight="1.8">
+              上記のボタンをクリックすることで、
+              <Text as="span" textDecoration="underline">
+                <Link to="/terms">利用規約</Link>
               </Text>
-              計算
+              および
+              <Text as="span" textDecoration="underline">
+                <Link to="/policy">プライバシーポリシー</Link>
+              </Text>
+              に同意するものとします。
             </Text>
           </Box>
-          <GoogleLoginButton
-            onClick={() => {
-              navigate('/signin', { state: { referrer: 'signin' } })
-            }}
-            disabled={false}
-          />
-          <Text fontSize="xs" align="center" color="gray.400" lineHeight="1.8">
-            上記のボタンをクリックすることで、
-            <Text as="span" textDecoration="underline">
-              <Link to="/terms">利用規約</Link>
-            </Text>
-            および
-            <Text as="span" textDecoration="underline">
-              <Link to="/policy">プライバシーポリシー</Link>
-            </Text>
-            に同意するものとします。
-          </Text>
-        </Box>
+        </Flex>
+        <Footer />
       </Flex>
-      <Footer />
-    </Flex>
+    </>
   )
 }
