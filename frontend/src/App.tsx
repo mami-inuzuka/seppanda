9f25a1983d5c334814d87e5d81293c3addd3365b
mami-inuzuka <inuzuka.mami@gmail.com>
2022-03-13 10:49:01 +0900

    Boxコンポーネントで定義していたスタイルをGlobalに移動

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,10 +11,8 @@ const App: VFC = () => (
   <ChakraProvider theme={theme}>
     <AuthProvider>
       <PaymentProvider>
-        <Box w="100%" minH="100vh" bg="gray.50">
-          <Box w="100%" m="0 auto" bg="white" maxW={{ base: 'auto', sm: '400px' }} minW="320px" minH="100vh">
-            <RouterConfig />
-          </Box>
+        <Box w="100%" m="0 auto" bg="white" maxW={{ base: 'auto', sm: '400px' }} minW="320px" minH="100vh">
+          <RouterConfig />
         </Box>
       </PaymentProvider>
     </AuthProvider>
