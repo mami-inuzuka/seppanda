10a83584de7f98230c4b5c78c284f9a67a4520a3
mami-inuzuka <inuzuka.mami@gmail.com>
2022-03-14 09:47:28 +0900

    NoPaymentListをNoPaymentMessageに変更

--- frontend/src/components/organisms/NoPaymentMessage.tsx
+++ frontend/src/components/organisms/NoPaymentMessage.tsx
@@ -4,7 +4,7 @@ import { Flex, Image, Text } from '@chakra-ui/react'
 
 import logomark from 'assets/images/logomark-gray-300.svg'
 
-export const NoPaymentList: VFC = () => (
+export const NoPaymentMessage: VFC = () => (
   <Flex direction="column" justify="center" align="center" pt={12}>
     <Text fontSize="sm" align="center" mb={4} color="gray.300">
       支払い情報はまだありません
