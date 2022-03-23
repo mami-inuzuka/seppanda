5e06433c6cc51a7b25cd4f7c6a8ee74ff6c18ed6
mami-inuzuka <inuzuka.mami@gmail.com>
2022-03-23 10:53:51 +0900

    Firebaseの環境変数を修正

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -8,7 +8,8 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
 initializeApp(firebaseConfig)
 
